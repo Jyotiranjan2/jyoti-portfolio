bad19877e5954058303dc8de2cd6be494755f5d3
Jyoti Ranjan Barik <104340440+Jyotiranjan2@users.noreply.github.com>
2025-08-26 12:13:14 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
 
 export default defineConfig({
+  base: "/",  // 👈 important for Vercel deploy
   plugins: [
     react(),
     runtimeErrorOverlay(),
